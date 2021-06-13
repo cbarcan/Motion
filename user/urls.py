91a3165994e6260f0b4bddd9e6a204c2156db874
Alexandru-Catalin Barcan <catalin.barcan@ymail.com>
2021-06-13 23:02:03 +0200

    my user view

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from user.views import UserList
+from user.views import UserList, MyUserList
 
 urlpatterns = [
     path(
         '',
         UserList.as_view()
+    ),
+    path(
+        'me/',
+        MyUserList.as_view()
     ),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+
 from user.serializers import ListUserSerializer
 
 User = get_user_model()
@@ -8,5 +9,14 @@ User = get_user_model()
 
 class UserList(ListAPIView):
     queryset = User.objects.all()
+    serializer_class = ListUserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MyUserList(ListAPIView):
+    def get_queryset(self):
+        user = self.request.user.id
+        return User.objects.filter(id=user)
+
     serializer_class = ListUserSerializer
     permission_classes = [IsAuthenticated]
