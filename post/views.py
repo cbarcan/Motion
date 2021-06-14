b72058b923ce017cf494a8d77df96b9da1c8a2a5
Solea83 <sophia.vonbank@icloud.com>
2021-06-14 17:07:31 +0200

    add view for likes and following and created serializers.

--- post/views.py
+++ post/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
 from motion_backend.permissions import IsOwnerOrReadOnly
 from .models import Post
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .serializers import CreatePostSerializer, PostSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView, ListAPIView
+from post.serializers.serializers import CreatePostSerializer, PostSerializer
 from rest_framework.permissions import IsAuthenticated
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class ListCreatePostView(ListCreateAPIView):
@@ -25,3 +30,42 @@ class GetUpdateDeletePostView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     lookup_field = 'id'
+
+
+class ToggleLikes(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def post(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+
+        if user in post.liked.all():
+            post.liked.remove(user)
+            return Response({'success': 'unliked'}, status=status.HTTP_200_OK)
+        else:
+            post.liked.add(user)
+            return Response({'success': 'liked'}, status=status.HTTP_200_OK)
+
+
+class ListUserLikedPosts(ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return self.request.user.liked_Posts
+
+
+class ListUserFollowingPosts(ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return self.request.user.following.Posts
+
+
+class ListUserPosts(ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return User.objects.get(pk=self.kwargs['id'].Posts)
+
+
