a7a641b2d2f5a348feab36d5587f8691e4a4d2c7
Solea83 <sophia.vonbank@icloud.com>
2021-06-22 12:02:33 +0200

    add user data to redux store

--- frontend/src/Store/Reducers/authReducer/index.js
+++ frontend/src/Store/Reducers/authReducer/index.js
@@ -10,6 +10,10 @@ const authReducer = (state, action) => {
     if (action.type === 'auth/login'){
         return { ...state, userToken: action.payload.access, userData: action.payload.user}
     }
+    if (action.type === 'userData'){
+        console.log('in userData reducer')
+        return { ...state, userData: action.payload}
+    }
     if (action.type === "setToken") {
         const newState = {...state};
         newState.userToken = action.payload;
