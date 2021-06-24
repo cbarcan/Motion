70ee6b90e66eb26dba868358347322ffaae2bb12
Solea83 <sophia.vonbank@icloud.com>
2021-06-24 18:04:27 +0200

    personalPosts kind of workin

--- frontend/src/Components/Profile/PersonalPosts.jsx
+++ frontend/src/Components/Profile/PersonalPosts.jsx
@@ -3,19 +3,45 @@ import { Main, FriendsStuff, Wrapper } from "./styled";
 import ProfileWall from ".";
 import Masonry from 'react-masonry-css';
 import Post from "../Post";
+import { useSelector } from 'react-redux';
 
 
 const PersonalPosts = () => {
-    const [posts, setPosts] = useState({}); 
+    const [posts, setPosts] = useState([]); 
+    const [userId, setUserId] = useState(-1);
+    const test = useSelector(state => state);
+    //console.log(test);
     
     const token = `Bearer ${localStorage.getItem("token")}`;
+
+    useEffect(() => {
+        const url = "https://motion.propulsion-home.ch/backend/api/users/me/";
+        let token = localStorage.token
+        token = `Bearer ${token}`
+
+        const config = {
+            method: "GET",
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "Authorization": token.toString()
+            }),
+        }
+    
+        const fetchId = async () => {
+          const res = await fetch(url, config);
+          const resData = await res.json();
+          setUserId(resData.id)
+          console.log(resData.id)
+        }
+        fetchId()
+    }, [])
+
 
     useEffect (() => {
         const headers = new Headers({
         "Authorization": token, 
         "Content-type": "application/json"
     }) // creating the headers
-
 
     const myInit = {
         method: 'GET',
@@ -24,9 +50,19 @@ const PersonalPosts = () => {
 
     fetch('https://motion.propulsion-home.ch/backend/api/social/posts/', myInit)
         .then(response => response.json())
-        .then(postsInfo => setPosts(postsInfo))
-    
+        .then(postsInfo => {
+            const myPosts = []
+            postsInfo.results.forEach((post) => {
+                if (post.user.id === userId) {
+                    myPosts.push(post);
+                }
+
+            })
+            setPosts(myPosts);
+        });
     }, [])
+
+    console.log(posts)
   
     return (
         <>
@@ -37,7 +73,9 @@ const PersonalPosts = () => {
                     breakpointCols={2}
                     className="my-masonry-grid"
                     columnClassName="my-masonry-grid_column">
-                    {posts && posts.results.map((post) => <Post key={post.id} post={post}/>)}
+                    {
+                    posts[0] ? posts.map((post) => <Post key={post.id} post={post}/>) : null
+                    }
                 </Masonry>
                 </Wrapper>
             </Main>
